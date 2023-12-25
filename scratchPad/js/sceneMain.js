a21d887ed4a60632c75df12d2ee0928b4badcc7f
Bong <borobong230@gmail.com>
2023-12-25 09:10:23 +0900

    add face setInteractive, pointerdown and pointerup

--- scratchPad/js/sceneMain.js
+++ scratchPad/js/sceneMain.js
@@ -7,6 +7,8 @@ class SceneMain extends Phaser.Scene {
       frameWidth: 120,
       frameHeight: 200,
     });
+    // image
+    this.load.image("face", "images/face.png");
   }
   create() {
     this.char = this.add.sprite(0, game.config.height / 2, "char");
@@ -27,6 +29,22 @@ class SceneMain extends Phaser.Scene {
       { fontFamily: "Anton", color: "#ff0000", fontSize: 48 }
     );
     this.text1.setOrigin(0.5, 0.5);
+
+    //image
+    this.face = this.add.image(
+      game.config.width / 2,
+      game.config.height / 2,
+      "face"
+    );
+    this.face.setInteractive();
+    this.face.on("pointerdown", this.onDown, this);
+    this.face.on("pointerup", this.onUp, this);
+  }
+  onUp() {
+    this.face.alpha = 1;
+  }
+  onDown() {
+    this.face.alpha = 0.5;
   }
   onCompleteHandler(tween, targets, custom) {
     var char = targets[0];
