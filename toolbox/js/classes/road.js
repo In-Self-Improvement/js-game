0b72c93fba69530241827a447e77f0429ff50ba9
Bong <borobong230@gmail.com>
2023-12-30 23:09:00 +0900

    feat add setSize in road.js

--- toolbox/js/classes/road.js
+++ toolbox/js/classes/road.js
@@ -5,5 +5,12 @@ class Road extends Phaser.GameObjects.Container {
     this.back = this.scene.add.image(0, 0, "road");
     this.add(this.back);
     this.scene.add.existing(this);
+
+    this.back.displayWidth = game.config.width * 0.5;
+    this.back.scaleY = this.back.scaleX;
+
+    this.setSize(this.back.displayWidth, game.config.height);
+
+    console.log(this);
   }
 }
